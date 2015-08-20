826c186097ef58376f4e112d3036a37c14a44e97
dipanjanc <dipanjan@qiddle.com>
2015-08-20 14:50:47 +0530

    added support for ubuntu

--- log_now.sh
+++ log_now.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
-
-#display SSID
+#display SSID Mac
 /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'
+#uncomment to display SSID for Ubuntu
+#iwgetid
 date && speedtest-cli --share && printf "\n\n================= \n\n\n"
