c79bf8f579c421515bebac8cf269fb6c724b4f66
dipanjanc <dipanjan@qiddle.com>
2015-08-29 21:47:02 +0530

    made user agnostic

--- log_now.sh
+++ log_now.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ST_CLI='/usr/local/bin/speedtest-cli'
-BBLOGGER_PATH="/Users/dipanjanchakraborty/bblogger"
+USER=$(whoami)
+BBLOGGER_PATH="/Users/$USER/bblogger"
 NOW=$(date +"%Y-%m-%d_%H-%M")
 TMP="$BBLOGGER_PATH/.temp"
 #YOUR_SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}')
