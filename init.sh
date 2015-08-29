4494ccbad15bd9741e7ab52d51b5047d3a415bb8
dipanjanc <dipanjan@qiddle.com>
2015-08-29 23:49:03 +0530

    init added

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+USER=$(whoami)
+BBLOGGER_PATH="/Users/$USER/bbspeedlogger"
+
+#add alias for bblogger in your zshrc
+printf 'alias lognow='/Users/$USER/bbspeedlogger/log_now.sh'' >> ~/.zshrc
+
+#Install dependencies
+pip install speedtest-cli 
+brew install terminal-notifier
+
+printf 'Done installing dependencies...Logging speed now'
+sh log_now.sh
+
+
