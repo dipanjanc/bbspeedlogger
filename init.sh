ffdfc95a064134d46d66b431635e60e4eeb7409f
dipanjanc <dipanjan@qiddle.com>
2015-08-30 17:33:27 +0530

    made zsh alias user agnostic

--- init.sh
+++ init.sh
@@ -2,14 +2,21 @@
 USER=$(whoami)
 BBLOGGER_PATH="/Users/$USER/bbspeedlogger"
 
-#add alias for bblogger in your zshrc
-printf 'alias lognow='/Users/$USER/bbspeedlogger/log_now.sh'' >> ~/.zshrc
-
 #Install dependencies
 pip install speedtest-cli 
 brew install terminal-notifier
 
-printf 'Done installing dependencies...Logging speed now'
+printf '\n\n creating symlink in usr/local/bin'
+
+sudo ln -s $BBLOGGER_PATH/log_now.sh /usr/local/bin/bbspeedlogger
+
+#add alias for bblogger in your zshrc
+
+printf '\n\n alias lognow='/usr/local/bin/bbspeedlogger'' >> ~/.zshrc
+
+printf '\n\n type lognow to log your broadband speed'
+
+printf '\n\n Done installing dependencies...Logging speed now'
 sh log_now.sh
 
 
