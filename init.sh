5d02494576ecc497eab2a686f0d3749395ce4999
dipanjanc <dipanjan@qiddle.com>
2015-09-24 17:09:11 +0530

    fixed alias duplication bug & added wget dependency installation

--- init.sh
+++ init.sh
@@ -5,18 +5,16 @@ BBLOGGER_PATH="/Users/$USER/bbspeedlogger"
 #Install dependencies
 pip install speedtest-cli 
 brew install terminal-notifier
-
-printf '\n\n creating symlink in usr/local/bin'
+brew install wget
+printf '\n\n creating symlink in usr/local/bin \n\n'
 
 sudo ln -s $BBLOGGER_PATH/log_now.sh /usr/local/bin/bbspeedlogger
 
 #add alias for bblogger in your zshrc
+grep -q -F 'alias bbspeedlogger='/usr/local/bin/bbspeedlogger'' ~/.zshrc || echo 'alias bbspeedlogger='/usr/local/bin/bbspeedlogger'' >> ~/.zshrcgrep -q -F 'alias bbspeedlogger='/usr/local/bin/bbspeedlogger'' ~/.zshrc || echo 'alias bbspeedlogger='/usr/local/bin/bbspeedlogger'' >> ~/.zshrc
+#printf '\n\n alias lognow='/usr/local/bin/bbspeedlogger'' >> ~/.zshrc
 
-printf '\n\n alias lognow='/usr/local/bin/bbspeedlogger'' >> ~/.zshrc
+printf '\n\n Done installing dependencies...type "lognow" to log your broadband speed\n\n'
 
-printf '\n\n type lognow to log your broadband speed'
-
-printf '\n\n Done installing dependencies...Logging speed now'
-sh log_now.sh
 
 
